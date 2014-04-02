056550e5b0298f350e66f6d636d5bf5ed1f210b2
Nicolas Demange <nicolas.demange@misys.com>
2014-04-02 17:15:55 +0200

    HUNT: Add lots of user services

--- site/2014/hunt/server/config.php
+++ site/2014/hunt/server/config.php
@@ -5,6 +5,8 @@ define('DB_NAME', 'alconisdad');
 define('DB_USER', 'alconisdad');
 define('DB_PASSWORD', '7jHbWDq9');
 
+define('USER_COOKIE', 'hunt_user');
+
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 
